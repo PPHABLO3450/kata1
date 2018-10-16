6871e935029c36b326139618a962eb432903eacd
Pablo <Pablo@192.168.1.38>
2018-10-16 19:49:25 +0100

    primera version

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,21 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package kata1;
 
-/**
- *
- * @author Pablo
- */
+import java.util.Date;
+
 public class Kata1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hola mundo");
+        
+        Person person = new Person("Pablo", new Date(49,8,24));
+        System.out.println(person.getName() + " tiene " + person.getAge());
     }
     
 }
